d625f8162382f96d99e77743dff5a05f340b55d9
Alpha-Pig <18351040@buaa.edu.cn>
2025-05-24 15:19:42 +0800

    Revert "重构Tgrad.lean的代码"
    This reverts commit 7423f6e27d098154248b5eac96f2d8e13d3116d8.

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ open Lake DSL
 package certigrad
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.11.0"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.11.0-rc1"
 
 
 @[default_target]
