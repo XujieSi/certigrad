11c3fcbb76084ac5fe8fe256e6aa251132be3577
Alpha-Pig <18351040@buaa.edu.cn>
2025-05-30 16:01:39 +0800

    Finish all the proof in  Tgrads

--- CertiGrad/SimpGrad.lean
+++ CertiGrad/SimpGrad.lean
@@ -448,3 +448,16 @@ lemma grad_bernoulli_neglogpdf₁ (k : TReal → TReal) (shape : S) (p z : T sha
     simp [T.smul.def, T.div_mul_inv]
     set A := (∇ (fun x => k x) (-(z * (eps shape + p).log + (1 - z) * (eps shape + (1 - p)).log).sum)).const shape
     ring
+
+
+lemma grad_bernoulli_neglogpdf₂ (k : TReal → TReal) (shape : S) (p z : T shape)
+                                (H_p₁ : 0 < p) (H_p₂ : 0 < 1 - p) (H_k : is_cdifferentiable k (bernoulli_neglogpdf p z)) :
+  ∇ (λ z => k (bernoulli_neglogpdf p z)) z = ∇ k (bernoulli_neglogpdf p z) •  (log (eps shape + (1 - p)) - log (eps shape + p)) := by
+  have H_diff₁ : is_cdifferentiable (λ (θ₀ : T shape) => k (-T.sum (θ₀ * T.log (eps shape + p) + (1 - z) * T.log (eps shape + (1 - p))))) z := by proveDifferentiable
+  have H_diff₂ : is_cdifferentiable (λ (θ₀ : T shape) => k (-T.sum (z * T.log (eps shape + p) + (1 - θ₀) * T.log (eps shape + (1 - p))))) z := by proveDifferentiable
+  unfold T.bernoulli_neglogpdf
+  rw [T.grad_binary (λ θ₁ θ₂=>k (- T.sum (θ₁ * T.log (eps shape + p) + (1 - θ₂) * T.log (eps shape + (1 - p))))) _ H_diff₁ H_diff₂]
+  simplifyGrad
+  simp [T.smul.def]
+  set A := (∇ (fun x => k x) (-(z * (eps shape + p).log + (1 - z) * (eps shape + (1 - p)).log).sum)).const shape
+  ring
